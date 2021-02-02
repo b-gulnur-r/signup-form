bcafd11ab21247a2dc3d288c8c605463c37742cf
Gulnur Burganieva <gulnur.r23@gmail.com>
2021-02-02 23:48:26 +0400

    fix label

--- src/ui/input/input-container.js
+++ src/ui/input/input-container.js
@@ -14,12 +14,13 @@ const Label = styled.label`
   font-size: 16px;
   font-weight: 500;
   color: ${(props) => props.theme.grey[0]};
+  width: 30%;
 `
 
 export const InputContainer = ({ id, label, children }) => {
   return (
     <Row>
-      <Label forname={id}>{label}</Label>
+      <Label htmlFor={id}>{label}</Label>
       {children}
       <Error id={id} />
     </Row>
